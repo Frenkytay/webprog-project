8bc40ae5f6f7d8b01e8d3002f3027e327fe4fc5b
Frenkytay <frenky.tay01@gmail.com>
2024-12-30 00:35:43 +0700

    vercel-5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,6 +14,7 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="css/styles.css" rel="stylesheet" />
     </head>
     <body id="page-top">
